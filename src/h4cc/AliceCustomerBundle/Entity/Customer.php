2d2f3b9ea44d58c2dc93cf797df3580ad952cf42
Julius Beckmann <github@h4cc.de>
2014-09-01 13:24:54 +0200

    Some Entites from another bundle and another entity manager for testing purposes. Missing files.

---
+++ src/h4cc/AliceCustomerBundle/Entity/Customer.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace h4cc\AliceCustomerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Customer
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Customer
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="address", type="text")
+     */
+    private $address;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Customer
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return Customer
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+}
---
+++ src/h4cc/AliceCustomerBundle/h4ccAliceCustomerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace h4cc\AliceCustomerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class h4ccAliceCustomerBundle extends Bundle
+{
+}
